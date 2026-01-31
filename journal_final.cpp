7f0abdb8b067ef858d86c99088c51d5e915b6221
Tobias Fajks <toby.fajks@gmail.com>
2026-01-31 15:51:53 +0100

    refactored some of the functions to be more readable and separated by client side and internal side

--- journal_final.cpp
+++ journal_final.cpp
@@ -1,111 +1,140 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include "journal_work.h"
+#include <vector>
 #define password "test123"
 #define notcancel(x) x != ""
 using namespace std;
 
-fstream entry("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entries.txt", fstream::app);
+enum MENU_ACTION {new_entry = 1, read_entry};
+fstream entry_file("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entries.txt", fstream::app);
 fstream key("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entry_key.txt", fstream::app);
 string login() 
 {
 	system("CLS");
+	string input;
 	cout << "                                   " << endl;
 	cout << "*____________ LOG  IN ____________*" << endl;
 	cout << "                                   " << endl;
 	cout << "       PASSWORD: ";
-	return getChoice();
+	cin >> input;
+	return input;
 }
-string menu()
+
+MENU_ACTION menu()
 {
+	int input;
 	system("CLS");
 	cout << "                                   " << endl;
 	cout << "*____________ JOURNAL ____________*" << endl;
 	cout << "|                                 |" << endl;
 	cout << "|          [1]NEW ENTRY           |" << endl;
 	cout << "|          [2]READ ENTRY          |" << endl;
-	return getChoice();
+	cin >> input;
+	return static_cast<MENU_ACTION>(input);
 }
-string entry_write(int decide) 
-{
+
+string getUserInputWithCancel(string userPrompt) {
 	//writing into entry and key
 	system("CLS");
+	string line;
 	cout << "to cancel, enter" << endl;
-	if(decide == 1)
-		cout << "begin your entry:" << endl;
-	else
-		cout << "Create a title for the new entry" << endl;
-	return write();
+	cout << userPrompt << endl;
+	getline(cin, line);
+	return line;
 }
-void read(int part, string look) 
+
+string getEntryText() 
+{
+	return getUserInputWithCancel("begin your entry:");
+}
+
+string getEntryTitle()
+{
+	cin.ignore();
+	return getUserInputWithCancel("Create a title for the new entry");
+}
+vector<string> getTitles() 
+{
+	vector<string> titles;
+	fstream key_read("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entry_key.txt");
+	for (string line; getline(key_read, line); )
+		titles.push_back(line);
+	return titles;
+}
+void printTitles(vector<string> title_list) 
 {
 	system("CLS");
-	string lines = "";
-	if (part == 1)
+	cout << "Your previous entries:" << endl;
+	for (string title : title_list)
+		cout << title << endl;
+}
+pair<string, string> getFullEntry(string entry_title) 
+{
+	string lines;
+	string title;
+	string entry;
+	fstream editing("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entries.txt");
+	int parts = 0;
+	while (getline(editing, lines))
 	{
-		//read all titles
-		fstream key_read("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entry_key.txt");
-		cout << "Your previous entries:" << endl;
-		for (string line; getline(key_read, line); )
-			cout << line << endl;
-	}
-	else 
-	{
-		//read from entries
-		fstream editing("C:\\Users\\user\\Desktop\\PROJEKTY\\CPP\\journal_final\\journal_final\\entries.txt");
-		string out;
-		int parts = 0;
-		while (getline(editing, lines))
+		if (lines == entry_title)
 		{
-			if (lines == look)
-			{
-				//reading title from entry
-				cout << "to return, enter" << endl;
-				cout << lines << endl;
-				parts++;
-			}
-			else if (parts == 1)
-			{
-				//reading the note part
-				parts = 0;
-				cout << lines << endl;
-				getline(cin, out);
-			}
+			parts++;
+			title = lines;
 		}
+		else if (parts == 1)
+		{
+			parts = 0;
+			entry = lines;
+			break;
+		}
+	}
+	return make_pair(title, entry);
+}
+void printEntry(const string title, const string entry) 
+{
+	system("CLS");
+	string input;
+	cout << title << endl;
+	cout << entry << endl;
+	getline(cin, input);
+}
+void writeNewEntryIntoFile(const string& title, const string& note) {
+	entry_file << title << endl;
+	key << title << endl;
+	entry_file << note << endl;
+}
+
+void createNewEntry() 
+{
+	string title = getEntryTitle();
+	if (notcancel(title))
+	{
+		string note = getEntryText();
+		if (notcancel(note))
+			writeNewEntryIntoFile(title, note);
 	}
 }
-void helper() 
+void readEntry() 
 {
-	string choice = menu();
-	system("CLS");
-	if (choice == "1")
+	string user_choice;
+	printTitles(getTitles());
+	cout << "enter entry name to read" << endl;
+	cin.ignore();
+	getline(cin, user_choice);
+	auto [title, entry] = getFullEntry(user_choice);
+	printEntry(title, entry);
+}
+void handleMenuActions(MENU_ACTION choice) 
+{	
+	if (choice == new_entry)
 	{
-		cin.ignore();
-		string title = entry_write(1);
-		if(notcancel(title))
-		{
-			//reading title if its not empty
-			string note = entry_write(2);
-			if (notcancel(note))
-			{
-				//if note and title arent empty, write them into files
-				entry << title << endl;
-				key << title << endl;
-				entry << note << endl;
-			}
-			note.clear();
-		}
-		title.clear();
+		createNewEntry();
 	}
-	else if (choice == "2")
+	else if (choice == read_entry)
 	{
-		string user_choice;
-		read(1, user_choice);
-		cout << "enter entry name to read" << endl;
-		cin.ignore();
-		getline(cin, user_choice);
-		read(2, user_choice);
+		readEntry();
 	}
 }
 int main()
@@ -117,6 +146,6 @@ int main()
 	}
 	while (true)
 	{
-		helper(); 
+		handleMenuActions(menu()); 
 	}
 }
